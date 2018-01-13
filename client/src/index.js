005fbfcdd80041b94d6d99b9551e802f04ec7644
Steven Meads <sbmeads@gmail.com>
2018-01-13 08:16:04 -0700

    add WebSocket link

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloLink, split } from 'apollo-client-preset'
+import { WebSocketLink } from 'apollo-link-ws'
+import { getMainDefinition } from 'apollo-utilities'
+
+const wsLink = new WebSocketLink({
+    uri: '[Subscriptions API URI]',
+    options: {
+      reconnect: true
+    }
+  })
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
