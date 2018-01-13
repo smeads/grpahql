11a45c91191e58468adc41900f4e3cf37b7a71a1
Steven Meads <sbmeads@gmail.com>
2018-01-13 08:22:40 -0700

    add split method and apollo client

--- client/src/index.js
+++ client/src/index.js
@@ -12,11 +12,27 @@ import { WebSocketLink } from 'apollo-link-ws'
 import { getMainDefinition } from 'apollo-utilities'
 
 const wsLink = new WebSocketLink({
-    uri: '[Subscriptions API URI]',
-    options: {
-      reconnect: true
-    }
-  })
+  uri: 'wss://subscriptions.us-west-2.graph.cool/v1/cjcdhfx3o0c0n0154be86lgre',
+  options: {
+    reconnect: true
+  }
+});
+
+const httpLink = new HttpLink({ uri: 'https://api.graph.cool/simple/v1/cjcdhfx3o0c0n0154be86lgre' });
+
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query)
+    return kind === 'OperationDefinition' && operation === 'subscription'
+  },
+  wsLink,
+  httpLink,
+);
+
+const client = new ApolloClient({
+  link,
+  cache: new InMemoryCache()
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
